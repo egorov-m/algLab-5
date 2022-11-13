c6a38b0f36b6c0b2a68f09ad07017b6700ed7604
Mikhail <m-5321e@yandex.com>
2022-11-13 23:41:20 +0500

    Added global exception handling

--- algLab-5/App.xaml.cs
+++ algLab-5/App.xaml.cs
@@ -13,5 +13,16 @@ namespace algLab_5
     /// </summary>
     public partial class App : Application
     {
+        /// <summary>
+        /// Глобальная обработка исключений.
+        /// WPF позволяет глобально обработать исключения, используя событие DispatcherUnhandledException в классе Application
+        /// </summary>
+        /// <param name="sender"> Объект события </param>
+        /// <param name="e"> Само событие </param>
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(e.Exception.Message, "Exception", MessageBoxButton.OK, MessageBoxImage.Error);
+            e.Handled = true;
+        }
     }
 }
